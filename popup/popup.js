692d0501b8919ec3abe9008521ae2410421dbec2
Luis Forero <foluis@hotmail.com>
2020-06-15 15:32:58 -0500

    ry to get info from rest api

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,46 @@
 $(function () {
 
+    let url = 'https://my-json-server.typicode.com/foluis/my-json-serve/clientCreditCard/5345346';
+
     $("#requestClientInfo").click(function () {
-        let url = 'https://my-json-server.typicode.com/foluis/my-json-serve/clientCreditCard/5345346';
-        //         // $('#requestClientInfo').attr('value', 'Si jala')
+
+        // $.get(url, function(data, status){
+        //     console.log("\nStatus: " + status);
+        //     console.log(data);
+        //     //alert("Data: " + data + "\nStatus: " + status);
+        //   });
+
         alert("This button will send a notification to the customer. So he can open an app in his device (mobile, tablet or pc) and enter his credit card information");
     });
 
     $("#getClientInfo").click(function () {
+
+        // let message = {
+        //     nameOnCard: "",
+        //     cardNumber: "",
+        //     cvs: "",
+        //     expirationMonth: "",
+        //     expirationYear: ""
+        // };
+
+        $.getJSON(url, function (result) {
+            //console.log(result);
+
+            // message.nameOnCard = "Luis Forero",
+            // message.cardNumber = "3141592653589793",
+            // message.cvs = "321",
+            // message.expirationMont = "12",
+            // message.expirationYear = "24"
+
+            // message.nameOnCard = result.cardNumber;
+            // message.cardNumber = result.cardNumber;
+            // message.cvs = result.cvs;
+            // message.expirationMont = result.expirationMont;
+            // message.expirationYear = result.expirationYear;
+        });
+
+        //console.log(message);
+
         let params = {
             active: true,
             currentWindow: true
@@ -23,7 +57,7 @@ $(function () {
                 expirationMonth: "12",
                 expirationYear: "24"
             };
-
+            console.log("From Pop up");
             console.log(message);
 
             chrome.tabs.sendMessage(tabs[0].id, message);
