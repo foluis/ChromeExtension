7f9f5f81a57f7311d7f88263e5e4bb82d5298f1c
Luis Forero <foluis@hotmail.com>
2020-06-15 14:35:20 -0500

    Add JQuery to Popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,34 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(function () {
 
-let APIKEY ='';
-
-    document.getElementById("requestClientInfo").addEventListener("click", function(){   
+    $("#requestClientInfo").click(function () {
         let url = 'https://my-json-server.typicode.com/foluis/my-json-serve/clientCreditCard/5345346';
-        // $('#requestClientInfo').attr('value', 'Si jala')
+        //         // $('#requestClientInfo').attr('value', 'Si jala')
         alert("This button will send a notification to the customer. So he can open an app in his device (mobile, tablet or pc) and enter his credit card information");
     });
 
-    document.getElementById("getClientInfo").addEventListener("click", function(){   
+    $("#getClientInfo").click(function () {
         let params = {
-            active:true,
-            currentWindow:true
+            active: true,
+            currentWindow: true
         }
 
         chrome.tabs.query(params, gotTabs);
 
-        function gotTabs(tabs){
+        function gotTabs(tabs) {
 
             let message = {
                 nameOnCard: "Luis Forero",
                 cardNumber: "3141592653589793",
-                cvs:"321",
-                expirationMonth:"12",
-                expirationYear:"24"
+                cvs: "321",
+                expirationMonth: "12",
+                expirationYear: "24"
             };
 
             console.log(message);
-        
+
             chrome.tabs.sendMessage(tabs[0].id, message);
         }
     });
-}, false);
+
+});
